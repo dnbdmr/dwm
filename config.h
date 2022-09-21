c50a75b4c8874e1402545b732f1635263d09356e
dnbdmr <dnbdmr@gmail.com>
2022-09-21 16:22:05 -0700

    Colors: rearrange variables

--- config.h
+++ config.h
@@ -9,19 +9,21 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 0;
+static const int focusonwheel       = 1;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#000000";
-static const char col_gray2[]       = "#080808";
-static const char col_gray3[]       = "#102017";
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#20e0a0";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray4, col_gray2, col_gray3 },
-	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
+static const char normbgcolor[]		= "#000000";
+static const char normbordercolor[]	= "#201020";
+static const char normfgcolor[]		= "#eeeeee";
+static const char selfgcolor[]		= "#000000";
+static const char selbordercolor[]	= "#40ff90";
+static const char selbgcolor[]		= "#20e0a0";
+static const char *colors[][3] = {
+       /*               fg           bg           border   */
+       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
+
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -41,6 +43,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.70; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -64,7 +67,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "x-terminal-emulator", NULL };
 
 #include <X11/XF86keysym.h>
