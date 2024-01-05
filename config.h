57e1b2d02af3bc7a27e4342ef154b8395a95b006
dnbdmr <dnbdmr@gmail.com>
2024-01-05 14:51:14 -0800

    Move pavucontrol and quodlibet to numbers

--- config.h
+++ config.h
@@ -34,9 +34,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 3,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Quodlibet",  NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Pavucontrol",  NULL,   NULL,       1 << 4,       0,           -1 },
+	{ "Quodlibet",  NULL,     NULL,       1 << 5,       0,           -1 },
 };
 
 /* layout(s) */
